afdb4fcff9238adee1ce374a65e218502f0e6a1b
Cinkafox <leofoxm40@gmail.com>
2023-03-02 20:08:46 +0300

    idnk

--- Readers/PrototypeReader/Type/index.js
+++ Readers/PrototypeReader/Type/index.js
@@ -1,8 +1,12 @@
 const reaction = require("./reaction")
 const reagent = require("./reagent")
+const decal = require("./decal")
+const tile = require("./tile")
 module.exports = (LANG) =>{
     return {
         reaction: new reaction(LANG),
-        reagent: new reagent(LANG)
+        reagent: new reagent(LANG),
+        decal: new decal(LANG),
+        tile: new tile(LANG)
     }
 }
