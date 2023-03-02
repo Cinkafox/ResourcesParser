ff5b0ed10916bfcba37e51a947e53eec29fc1ab3
Cinkafox <leofoxm40@gmail.com>
2023-03-02 08:57:13 +0300

    Add value variable

--- Type/reaction.js
+++ Type/reaction.js
@@ -1,11 +1,12 @@
 class reaction{
     Reactions = {}
     Add_item(item){
+        if(!item.products) return
         let reactants = {}
         for (let a of Object.keys(item.reactants)){
             reactants[a] = item.reactants[a].amount
         }
-        this.Reactions[item.id] = reactants
+        this.Reactions[item.id] = { reactants, col: item.products[item.id] ? item.products[item.id] : '1' }
     }
 }
 
