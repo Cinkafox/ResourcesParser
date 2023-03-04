e2f9de209e0b83b6c5195cc9051f7916c33c473b
Cinkafox <leofoxm40@gmail.com>
2023-03-04 16:07:24 +0300

    Change SpriteComponent

--- Readers/ComponentReader/Components/SpriteComponent.js
+++ Readers/ComponentReader/Components/SpriteComponent.js
@@ -3,23 +3,29 @@ const path = require("path")
 
 class SpriteComponent{
     SpritePath = ""
-    Layers = []
-
-    Read(ComponentItem){
+    Layers = { BelowFloor: [], FloorObjects: [], FloorTiles: [], ThickPipe: [], ThinPipe: [], ThickWire: [], ThinWire: [], Walls: [], WallMountedItems: [], WallTops: [], Objects: [], Mobs: [], Items: [], SmallObjects: [], Doors: [], Overdoors: []}
+    
+    Read(ComponentItem,ParentComponentItem){
+        const Depth = ComponentItem.drawdepth ? ComponentItem.drawdepth : "Objects"
         if(ComponentItem.sprite){
             this.SpritePath = ComponentItem.sprite
-            this.Layers.push(this.LayerReader(ComponentItem))
+            this.Layers[Depth].push(this.LayerReader(ComponentItem))
         }
         if(ComponentItem.layers) 
             for(const LayerItem of ComponentItem.layers){
                 const Layer = this.LayerReader(LayerItem)
-                if (Layer) this.Layers.push(Layer)
+                if (Layer) this.Layers[Depth].push(Layer)
             }
-                
-        return { ...this.Layers }
+        if(ParentComponentItem && ParentComponentItem.Sprite){
+            for(let o of Object.keys(ParentComponentItem.Sprite)){
+                this.Layers[o] = [ ...ParentComponentItem.Sprite[o], ...this.Layers[o]]
+            }
+        }
+        return this.Layers
     }
 
     LayerReader(SpriteItem){
+        //if (SpriteItem.visible == 'false') return
         let LocalSpritepath = this.SpritePath
         if(SpriteItem.sprite) LocalSpritepath = SpriteItem.sprite
         if(!LocalSpritepath) return
--- Readers/ComponentReader/index.js
+++ Readers/ComponentReader/index.js
@@ -5,19 +5,20 @@ class ComponentReader {
     constructor(Prototype) {
         this.Prototype = Prototype
     }
-    ComponentSelector(ComponentItem) {
+    ComponentSelector(ComponentItem,ParentComponents) {
 
         const LocalComponent = ComponentsClass[ComponentItem.type]
-        if (LocalComponent) return new LocalComponent().Read(ComponentItem)
+        if (LocalComponent) return new LocalComponent().Read(ComponentItem, ParentComponents)
         return new ComponentsClass.BaseComponent().Read(ComponentItem)
     }
     ComponentReader(item) {
         let components = {}
         if (item == undefined) return components
-        if (item.parent) components = this.ComponentReader(this.Prototype.TYPES.entity.Items[item.parent])
+        const parent = this.Prototype.TYPES.entity.Items[item.parent]
+        if (parent) components = this.ComponentReader(this.Prototype.TYPES.entity.Items[item.parent])
         if (item.components)
             for (let o of item.components)
-                components[o.type] = this.ComponentSelector(o)
+                components[o.type] = this.ComponentSelector(o, components)
         return components
     }
 }
